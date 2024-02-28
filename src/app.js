6d6ba638350c97f789b9d89673ed7db08c6b983a
Welton Rodrigues <96987306+weltondev@users.noreply.github.com>
2024-02-28 11:01:08 -0300

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -1,15 +1,18 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const connectionDB = require('./database/connectionDB');
-const app = express();
 
+const app = express();
 const routes = require('./routes/routes');
+const PORT = process.env.PORT
+
 
 app.use(express.json());
 app.use(cors());
 app.use(routes);
 connectionDB();
 
-app.listen(3000, ()=> {
+app.listen(PORT, ()=> {
   console.log(`Servidor esta no ar!`);
 });
--- src/database/connectionDB.js
+++ src/database/connectionDB.js
@@ -1,8 +1,12 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
+const DB_USER = process.env.DB_USER;
+const DB_PASS = process.env.DB_PASS;
+
 
 function connection(){
   mongoose.connect(
-    `mongodb+srv://admin:admin123@cluster0.oelrajq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+    `mongodb+srv://${DB_USER}:${DB_PASS}@cluster0.oelrajq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
   )
   .then(()=>{
     console.log(`Conectado ao MongoDB!`);
